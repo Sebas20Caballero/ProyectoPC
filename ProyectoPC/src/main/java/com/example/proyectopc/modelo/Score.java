c5d5723f6a55949d7d428dc0041d43faba306f29
unknown <jhoan.caballero.mt@usa.edu.co>
2022-10-08 10:35:15 -0500

    Subir mi repositorio

---
+++ ProyectoPC/src/main/java/com/example/proyectopc/modelo/Score.java
@@ -0,0 +1,51 @@
+package com.example.proyectopc.modelo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="score")
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idScore;
+    private Integer cal;
+    private String messageText;
+
+    @OneToOne
+    private Reservation reservation;
+
+    public Integer getIdScore() {
+        return idScore;
+    }
+
+    public void setIdScore(Integer idScore) {
+        this.idScore = idScore;
+    }
+
+    public Integer getCal() {
+        return cal;
+    }
+
+    public void setCal(Integer cal) {
+        this.cal = cal;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+}
